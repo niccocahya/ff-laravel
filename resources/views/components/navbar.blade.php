ff269c4fd429c1e44e07986c7339b2ba032783fb
Nicco Cahya <niccocahya883@gmail.com>
2026-02-17 11:23:15 +0700

    nicco 1

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <nav class="w-full h-16 bg-[#275CAA] flex flex-row p-3 px-20 justify-between fixed z-50">
+        <img src="{{ asset('images/logo-ff.png') }}" alt="logo-ff">
+        <div class="nav-right flex items-center gap-4">
+            <div class="lang flex text-xs gap-1 items-center text-[#F7FFFA]">
+                <a href="#" class="font-semibold hover:underline underline-offset-2 ">EN</a>
+                <h1>|</h1>
+                <a href="#" class="hover:underline underline-offset-2">ID</a>
+            </div>
+            <button
+                class="px-8 bg-[#F7FFFA] border border-transparent hover:border-[#F7FFFA] hover:text-[#F7FFFA] transition-all duration-200 hover:bg-[#275CAA] text-[#275CAA] rounded-lg font-semibold p-1.5">Apply
+                Now</button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-menu-icon lucide-menu">
+                <path d="M4 5h16" />
+                <path d="M4 12h16" />
+                <path d="M4 19h16" />
+            </svg>
+        </div>
+    </nav>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,26 +13,7 @@
 
 <body>
     <div class="wrapper-all w-full min-h-screen bg-[#F7FFFA] relative">
-        <nav class="w-full h-16 bg-[#275CAA] flex flex-row p-3 px-20 justify-between fixed z-50">
-            <img src="{{ asset('images/logo-ff.png') }}" alt="logo-ff">
-            <div class="nav-right flex items-center gap-4">
-                <div class="lang flex text-xs gap-1 items-center text-[#F7FFFA]">
-                    <a href="#" class="font-semibold hover:underline underline-offset-2 ">EN</a>
-                    <h1>|</h1>
-                    <a href="#" class="hover:underline underline-offset-2">ID</a>
-                </div>
-                <button
-                    class="px-8 bg-[#F7FFFA] border border-transparent hover:border-[#F7FFFA] hover:text-[#F7FFFA] transition-all duration-200 hover:bg-[#275CAA] text-[#275CAA] rounded-lg font-semibold p-1.5">Apply
-                    Now</button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="lucide lucide-menu-icon lucide-menu">
-                    <path d="M4 5h16" />
-                    <path d="M4 12h16" />
-                    <path d="M4 19h16" />
-                </svg>
-            </div>
-        </nav>
+        <x-navbar />
         <div class="main-home flex flex-col gap-24 pt-16">
             <div class="heroes w-full h-[80vh] flex items-center">
                 <div class="w-[33%] flex flex-col justify-center gap-10 px-14 h-full text-sm ">
